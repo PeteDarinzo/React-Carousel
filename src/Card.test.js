f6d80e8c6a9e4195bb553d6fa5102211779bf543
Peter Darinzo <birel44@yahoo.com>
2021-12-29 15:10:47 -0700

    first carousel commit

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
-import App from "./App";
+import Card from "./Card";
 
-it("renders without crashing", function () {
-  render(<App />);
+
+it("renders a carousel without crashing", function () {
+  render(<Card />);
 })
 
 it("matches the snapshot", function () {
-  const { asFragment } = render(<App />);
+  const { asFragment } = render(<Card />);
   expect(asFragment()).toMatchSnapshot();
 })
